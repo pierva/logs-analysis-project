5d340e921b1b407df9ef15525cf97197dd7c4468
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-01-31 02:15:29 -0500

    feat: Add SQL to get most popular authors

--- newsdb.py
+++ newsdb.py
@@ -6,9 +6,24 @@ def get_articles():
     db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
     cursor.execute(
-        "SELECT title, count(*) as num from articles " +
+        "SELECT title, count(*) as num FROM articles " +
         "JOIN log ON (REPLACE(log.path, '/article/', '') = articles.slug) " +
-        "GROUP BY title ORDER BY num DESC LIMIT 3")
+        "GROUP BY title ORDER BY num DESC LIMIT 3"
+    )
     articles = cursor.fetchall()
     db.close()
     return articles
+
+# Who are the most popular article authors of all time?
+def get_popular_author():
+    db = psycopg2.connect("dbname=news")
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT name, count(*) as num FROM authors " +
+        "INNER JOIN articles ON (authors.id = articles.author) " +
+        "INNER JOIN log ON (REPLACE(log.path, '/article/', '') = articles.slug) " +
+        "GROUP BY name ORDER BY num DESC"
+    )
+    authors = cursor.fetchall()
+    db.close()
+    return authors
