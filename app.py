fbd0b4f6f5095b8d780b8ffbd4cbdc32ed3b7389
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-01-31 04:06:54 -0500

    feat: Add app logic and rename file
    Create small console interface to ask the user what question (query) the
    program has to answer.

---
+++ app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from newsdb import get_articles, get_popular_author, get_errors
+import datetime
+
+print("Please choose from these questions:")
+print("1 - What are the most popular three articles of all time?")
+print("2 - Who are the most popular article authors of all time?")
+print("3 - On which days did more than 1% of requests lead to errors?")
+print("Enter q or quit to exit.\n")
+
+while True:
+    selection = input("Enter your selection (num): ").strip().lower()
+    if selection == "q" or selection == "quit":
+        print("Goodbye.")
+        break
+    else:
+        if selection == "1":
+            articles = get_articles()
+            for article in articles:
+                print("%s - %s views" % (article[0], article[1]))
+            print()
+        elif selection == "2":
+            authors = get_popular_author()
+            for author in authors:
+                print("%s - %s views" % (author[0], author[1]))
+            print()
+        elif selection == "3":
+            errorDay = get_errors()
+            percentage = "{:.1f}".format((errorDay[0][1] * 100)/errorDay[0][2])
+            print(errorDay[0][0].strftime("%B %d, %Y") + " - " +
+                  percentage + "% errors\n")
+        else:
+            print("Sorry, I didn't get your question. Try again.")
--- news.py
+++
@@ -1,3 +0,0 @@
-import psycopg2
-
-db = psycopg2.connect("dbname=news")
